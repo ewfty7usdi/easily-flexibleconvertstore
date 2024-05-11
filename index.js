bf0960b011de807c829ead133681b29c935dd7da
ewfty7usdi <gs1ematic75@hotmail.com>
2024-05-11 11:16:14 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const dropRight = (arr, n = 1) => arr.slice(0, -n);
+function maxArea(height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    maxArea = Math.max(
+      maxArea,
+      Math.min(height[left], height[right]) * (right - left),
+    );
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return maxArea;
+}
